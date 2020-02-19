50ee0bfa41dd22d19a15c8f271d81c08cb5a0db9
varya <hallovarvara@gmail.com>
2020-02-19 23:57:03 +0300

    fix: solve task 6: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,35 +1,39 @@
 module.exports = function transform(arr) {
+  
   if (!Array.isArray(arr)) throw new Error();
 
-  let notSimple = false;
-  for(let i=0; i < arr.length; i++) {
-      if(arr[i] === '--discard-next' ||
-         arr[i] === '--discard-prev' ||
-         arr[i] === '--double-next' ||
-         arr[i] === '--double-prev') notSimple = true; break;
-  }
-
-  if (arr.length == 0 || !notSimple) return arr;
-
   let result = [];
-  arr.reduce( (deleteNext, current, i) => {
-    if (current === '--discard-next' && arr[i + 1])     deleteNext = true;
-    else if (current === '--discard-prev' && result[i - 1])  delete result[i - 1];
-    else if (current === '--double-next' && arr[i + 1])      result.push(arr[i + 1]);
-    else if (current === '--double-prev' && result[i - 1])   result.push(result[i - 1]);
-    else {
-        switch (current) {
-            case '--discard-next':
-            case '--discard-prev':
-            case '--double-next':   
-            case '--double-prev':
-                    break;
-            default: result.push(current);
-        }
+  
+  for (let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
+      case '--discard-next':
+      case '--discard-prev':
+      case '--double-next':
+      case '--double-prev':
+        continue;
     }
 
-    if (deleteNext && result[i]) delete result[i];
-  }, 0);
+    result.push(arr[i]);
+
+    switch(arr[i+1]) {
+      case '--double-prev':
+        result.push(arr[i]);
+        break;
+      case '--discard-prev':
+        result.splice(result.length-1,1);
+        break;
+    }
+
+    switch(arr[i-1]) {
+      case '--double-next':
+        result.push(arr[i]);
+        break;
+      case '--discard-next':
+        result.splice(result.length-1,1);
+        break;
+    }
+  }
 
   return result;
+
 };
